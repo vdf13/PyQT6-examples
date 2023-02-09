d66345410019ec0beadce4acaff5cd6cc1cae23b
vdf13 <vdf13@free.fr>
2023-02-09 16:30:16 +0100

    dialog and main_window GUI file

--- hello.py
+++ hello.py
@@ -3,3 +3,21 @@
 import sys
 
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget
+
+# Create an instance of QApplication
+app = QApplication([])
+
+
+# Create the application GUI
+window = QWidget()
+window.setWindowTitle("PyQT Application")
+window.setGeometry(100,100,280,80)
+helloMsg = QLabel("<h1>Hello World!</h1>", parent=window)
+helloMsg.move(60, 15)
+
+# Show the application GUI
+window.show()
+
+# Run the application's event loop
+sys.exit(app.exec())
+
